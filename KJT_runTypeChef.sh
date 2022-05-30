175274908fdc6df21e79e7cec70a694d5d8ba879
Kevin Jedelhauser <kevin.jedelhauser@uni-ulm.de>
2022-05-30 16:39:09 +0200

    should now run on git repo

---
+++ KJT_runTypeChef.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./cleanBusyboxGit.sh
+
+./analyzeBusyboxGit.sh | tee jedelhauserGit.log
+ 
---
+++ KJT_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "Preparing features, feature model, and presence conditions"
+
+# Choose Java 8 (type 1)
+sudo update-alternatives --config java
+./KJT_prepare.sh 
+
+echo -e "Run TypeChef on busybox"
+
+# Choose Java 17 (type 0)
+sudo update-alternatives --config java
+./jedelhauserGIT.sh
